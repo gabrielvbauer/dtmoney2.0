d575766e7cd10179e47a91e7ba0251d941739c80
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2023-02-15 20:39:43 -0300

    feat: formatter

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
-import { priceFormatter } from "../../utils/formatter";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
@@ -23,11 +23,11 @@ export function Transactions() {
               <tr key={transaction.id}>
                 <td width="50%">{transaction.description}</td>
                 <PriceHighlight variant={transaction.type}>
-                  {transaction.type === 'outcome' && '-'}
+                  {transaction.type === 'outcome' && '- '}
                   {priceFormatter.format(transaction.value)}
                 </PriceHighlight>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
               </tr>
             ))}
           </tbody>
